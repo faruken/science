ecded0a44565237016b0c0802330887d1a1cfaf8
Faruk Akgul <faruken@users.noreply.github.com>
2017-01-23 08:20:22 +1100

    test tools

---
+++ tests/test_tools.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from typing import (Dict, Union, List)
+
+from tools.protocols import Protocol
+from tools.utils import imgur_parser
+
+
+class TestProtocol(unittest.TestCase):
+    def setUp(self) -> None:
+        self.protocol: Protocol = Protocol(200, "Hello World")
+        self.urls: List[str] = ["http://i.imgur.com/531SUvC.jpg",
+                                "http://example.com/b.jpg",
+                                "http://i.imgur.com/2M11zXf.gifv",
+                                "http://imgur.com/531SUvC",
+                                "imgur.com/531SUvC",
+                                "http://imgur.com/a/531SUvC",
+                                "http://imgur.com/a/531SUvC.jpg"]
+
+    def test_protocol__asdict(self) -> None:
+        mapp: Dict[str, Union[str, int]] = self.protocol._asdict()
+        self.assertDictEqual({"status": 200, "message": "Hello World"}, mapp)
+
+    def test_imgur_parser(self) -> None:
+        self.assertIsNone(imgur_parser(None))
+        self.assertEqual(self.urls[0], imgur_parser(self.urls[0]))
+        for url in self.urls[1:]:
+            self.assertIsNone(imgur_parser(url))
