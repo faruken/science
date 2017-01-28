fc0f60bf46993fe6f520d915e094dc23df7fc06f
Faruk Akgul <faruken@users.noreply.github.com>
2017-01-28 16:50:59 +1100

    log backend

--- science/config.py
+++ science/config.py
@@ -15,6 +15,7 @@ class Config:
     TESTING: bool = False
     LOGLEVEL: int = logbook.DEBUG
     PORT: int = 4000
+    log_backend: str = "127.0.0.1"
     broker_url: str = "redis://127.0.0.1:6379/0"
     result_backend: str = "redis://127.0.0.1:6379/0"
     aws_access_key_id: Optional[str] = None
@@ -28,6 +29,7 @@ class AWSConfig(Config):
     LOGLEVEL: int = logbook.INFO
     aws_access_key_id: Optional[str] = os.environ.get("AWS_ACCESS_KEY_ID")
     aws_access_secret_id: Optional[str] = os.environ.get("AWS_ACCESS_SECRET_ID")
+    log_backend: str = os.environ.get("AWS_LOG_BACKEND")
     broker_url: str = os.environ.get("AWS_BROKER_URL")
     result_backend: str = os.environ.get("AWS_RESULT_BACKEND")
 
@@ -43,6 +45,7 @@ class DockerComposeConfig(Config):
     """DEBUG is `true` since it's only used for development
     """
     DEBUG: bool = True
+    log_backend: str = "redis"
     broker_url: str = "redis://:redis_passwdd@redis"
     result_backend: str = "redis://:redis_passwdd@redis"
 
